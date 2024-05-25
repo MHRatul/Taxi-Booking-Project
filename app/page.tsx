20201b8e8bd0d5b0b419cf96dc0785161b79f1e2
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-05-25 17:53:30 +0600

    NavBar component added & make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-       <UserButton afterSignOutUrl="/" />
+       
        <h2> Hello World</h2>
    </div>
   )
