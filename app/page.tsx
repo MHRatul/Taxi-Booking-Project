d238e7e9faefe60cd6dd685f1a879e5ff52bb171
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-05-25 15:55:28 +0600

    create  Clerk Sign-in/Sign-Up authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
+       <UserButton afterSignOutUrl="/" />
        <h2> Hello World</h2>
    </div>
   )
