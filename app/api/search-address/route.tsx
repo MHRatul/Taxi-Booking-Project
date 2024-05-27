cbbffd281af8aa44c112f18fb910d6abd6782870
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-05-27 10:15:34 +0600

    session Token Change for rechech

--- app/api/search-address/route.tsx
+++ app/api/search-address/route.tsx
@@ -4,7 +4,7 @@ export async function GET(request:any) {
     const {searchParams} = new URL(request.url);
     const searchText=searchParams.get('q');
 
-    const res=await fetch(BASE_URL+'?q='+searchText+'?language=en&limit=10&session_taken=0e5c3adf-722b-4a11-88fb-4ff272300bbb&country=US'
+    const res=await fetch(BASE_URL+'?q='+searchText+'=Michigan%20Stadium?language=en&limit=8&session_taken=f6193827-9ef4-43e5-acf6-3d4a1c466587&country=US'
     +"&access_token="+process.env.MAPBOX_ACCESS_TOKEN,
         {
             headers:{
